e4a00294f33b0430011061df3cfee337c70112dd
DevRamona <r.ingabire@alustudent.com>
2024-07-20 20:19:47 +0200

    new changes

--- vite-project/src/Components/Cards.jsx
+++ vite-project/src/Components/Cards.jsx
@@ -8,6 +8,7 @@ export default function Cards({img, heading1, paragraph, heading2}) {
                 <h1>{heading1}</h1>
                 <p>{paragraph}</p>
                 <h2>{heading2}</h2>
+                <h3>{heading2}</h3>
 
             </div>
 
